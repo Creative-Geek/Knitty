26b6cee162277a2363f162ae1afbe321a7b3bbfd
Creative-Geek <ahmedtaha1234@gmail.com>
2025-09-24 09:19:15 +0300

    Second Run

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello from knitty!")
+    print("Main project is still under development!")
+    print("You should head to the notebook.ipynb to get started.")
+
+
+if __name__ == "__main__":
+    main()
